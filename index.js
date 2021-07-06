a25c0db60e7e05a0171e139ada7be402c005157c
Sergiolm17 <kingsergiolm17@gmail.com>
2021-07-06 18:53:48 -0500

    last update

--- index.js
+++ index.js
@@ -1,49 +1,112 @@
-const {
-	Extension,
-	INPUT_METHOD,
-	PLATFORMS,
-} = require('deckboard-kit');
-const fetch = require('node-fetch');
+const { dialog } = require("electron");
+
+const { Extension, INPUT_METHOD, PLATFORMS } = require("deckboard-kit");
+const fetch = require("node-fetch");
 
 class CallUrl extends Extension {
-	constructor() {
-		super();
-		this.name = 'Deckboard Call Url';
-		this.platforms = [PLATFORMS.WINDOWS, PLATFORMS.MAC];
-		this.configs = null;
-		this.inputs = [
-			{
-				label: 'URL to Call',
-				value: 'url-to-call',
-				icon: 'link',
-				mode: 'custom-value',
-				fontIcon: 'fas',
-				color: '#ff29df',
-				input: [
-					{
-						label: 'URL to call',
-						ref: 'urlToCall',
-						type: INPUT_METHOD.INPUT_TEXT
-					}
-				]
-			}
-		];
-	}
+  constructor(props) {
+    super();
+    this.setValue = props.setValue;
 
-	// Executes when the extensions loaded every time the app start.
-	initExtension() {
-		return;
-	}
+    this.name = "FIREBASE";
+    this.platforms = [PLATFORMS.WINDOWS];
+    this.configs = {
+      steamPaths: {
+        type: "text",
+        name: "FIREBASE DATABASE URL",
+        descriptions: "",
+        value: "https://gente-mala.firebaseio.com/deckboard.json",
+      },
+    };
+    this.isFirstInit = true;
 
-	execute(action, args) {
-		switch (action) {
-			case 'url-to-call':
-				fetch(args.urlToCall);
-				break;
-			default:
-				break;
-		}
-	};
+    this.inputs = [
+      {
+        label: "SAVE DATA",
+        value: "save-data",
+        icon: "save",
+        input: [
+          {
+            label: "TARGET",
+            ref: "target",
+            type: INPUT_METHOD.INPUT_TEXT,
+          },
+          {
+            label: "DATA",
+            ref: "data",
+            type: INPUT_METHOD.INPUT_TEXT,
+          },
+        ],
+      },
+      {
+        label: "GET DATA",
+        value: "get-data",
+        icon: "download",
+        mode: "custom-value",
+        color: "#1a1a1a",
+        input: [
+          {
+            label: "Selecciona el dato",
+            type: "input:select",
+            items: [
+              { value: "text1", label: "texto 1" },
+              { value: "text2", label: "texto 2" },
+            ],
+          },
+        ],
+      },
+    ];
+  }
+
+  // Executes when the extensions loaded every time the app start.
+  initExtension() {
+    this.updateClock();
+    if (this.isFirstInit) this.isFirstInit = false;
+    this.updateClock();
+    /*
+    setInterval(
+      () => {
+      },
+      this.isFirstInit ? 60000 - moment().get("seconds") * 1000 : 60000,
+    );
+    */
+  }
+  async updateClock() {
+    this.setValue({
+      text1: "texto 1",
+      text2: "texto 2",
+    });
+    fetch(url, callback)
+      .then((resp) => resp.json())
+      .then(({ data }) => {
+        console.log(data);
+        dialog.showMessageBox("hola");
+      });
+  }
+
+  execute(action, args) {
+    switch (action) {
+      case "save-data":
+        const putMethod = {
+          method: "PUT", // Method itself
+          headers: {
+            "Content-type": "application/json; charset=UTF-8", // Indicates the content
+          },
+          body: JSON.stringify({
+            [args.target]: args.data,
+          }), // We send data in JSON format
+        };
+        fetch(this.configs.steamPaths.value, putMethod);
+        break;
+      case "get-data":
+        dialog.showMessageBox("hola");
+
+        this.updateClock();
+        break;
+      default:
+        break;
+    }
+  }
 }
 
-module.exports = sendData => new CallUrl(sendData);
+module.exports = (sendData) => new CallUrl(sendData);
